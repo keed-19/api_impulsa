dca5acb137f47469180963bf391f35bbbe577512
Kevin Mazariegos <keedgmz19@gmail.com>
2022-02-25 12:33:50 -0600

    Creación de las funcionalidades de Impulsa}

--- build/src/api/controllers/ImpulsaController.js
+++ build/src/api/controllers/ImpulsaController.js
@@ -20,73 +20,6 @@ var path = require('path');
 /** My class of user controller */
 class ImpulsaController {
     constructor() {
-        //probando la subida de archivos pdf
-        this.Savefiles = (_req, res) => __awaiter(this, void 0, void 0, function* () {
-            var _a, _b;
-            res.set('Access-Control-Allow-Origin', '*');
-            // let file = _req.files;
-            // var file:Array<any> = _req.files as any
-            // var urlFile:Array<any>=[] 
-            // console.log(urlFile)
-            // file.forEach(item=>{ 
-            // urlFile.push(
-            // {
-            //     "url":item.path,
-            // });
-            // });
-            // console.log(urlFile)
-            var file = _req.file;
-            if (!file) {
-                const error = new Error('Please upload a file');
-                return error;
-            }
-            else if (file.mimetype === 'application/pdf') {
-                /** search Number phone in the data base */
-                const isUserExist = yield User_1.UsersModel.findOne({ username: _req.params.phoneNumber });
-                if (isUserExist) {
-                    //instantiating the model for save data
-                    const user = new InsurancePolicy_1.InsurancePoliciesModel({
-                        insurerName: _req.body.insurerName,
-                        policyNumber: _req.body.policyNumber,
-                        policyType: _req.body.policyType,
-                        effectiveDate: Date.now(),
-                        expirationDate: Date.now(),
-                        status: _req.body.status,
-                        fileUrl: file.filename,
-                        clientId: isUserExist.clientId
-                    });
-                    try {
-                        //save data
-                        const savedUser = yield user.save();
-                        //send request exit
-                        res.status(200).json({
-                            message: 'Poliza registrada',
-                            file
-                        });
-                    }
-                    catch (error) {
-                        res.status(404).json({
-                            error,
-                            status: 404
-                        });
-                    }
-                }
-                else {
-                    fs_1.default.unlinkSync(`${(_a = _req.file) === null || _a === void 0 ? void 0 : _a.path}`);
-                    res.status(400).json({
-                        message: 'Usuario no encontrado',
-                        status: 400,
-                    });
-                }
-            }
-            else {
-                fs_1.default.unlinkSync(`${(_b = _req.file) === null || _b === void 0 ? void 0 : _b.path}`);
-                res.status(400).json({
-                    message: 'no es un archivo pdf',
-                    status: 400,
-                });
-            }
-        });
         //ver pdf de un cliente
         this.ViewFile = (_req, res) => __awaiter(this, void 0, void 0, function* () {
             res.set('Access-Control-Allow-Origin', '*');
@@ -148,6 +81,73 @@ class ImpulsaController {
                     res.json("success");
                 }
             });
+        });
+        //probando la subida de archivos pdf
+        this.Savefiles = (_req, res) => __awaiter(this, void 0, void 0, function* () {
+            var _a, _b;
+            res.set('Access-Control-Allow-Origin', '*');
+            // let file = _req.files;
+            // var file:Array<any> = _req.files as any
+            // var urlFile:Array<any>=[] 
+            // console.log(urlFile)
+            // file.forEach(item=>{ 
+            // urlFile.push(
+            // {
+            //     "url":item.path,
+            // });
+            // });
+            // console.log(urlFile)
+            var file = _req.file;
+            if (!file) {
+                const error = new Error('Please upload a file');
+                return error;
+            }
+            else if (file.mimetype === 'application/pdf') {
+                /** search Number phone in the data base */
+                const isUserExist = yield User_1.UsersModel.findOne({ username: _req.params.phoneNumber });
+                if (isUserExist) {
+                    //instantiating the model for save data
+                    const user = new InsurancePolicy_1.InsurancePoliciesModel({
+                        insurerName: _req.body.insurerName,
+                        policyNumber: _req.body.policyNumber,
+                        policyType: _req.body.policyType,
+                        effectiveDate: Date.now(),
+                        expirationDate: Date.now(),
+                        status: _req.body.status,
+                        fileUrl: file.filename,
+                        clientId: isUserExist.clientId
+                    });
+                    try {
+                        //save data
+                        yield user.save();
+                        //send request exit
+                        res.status(200).json({
+                            message: 'Poliza registrada',
+                            file
+                        });
+                    }
+                    catch (error) {
+                        res.status(404).json({
+                            error,
+                            status: 404
+                        });
+                    }
+                }
+                else {
+                    fs_1.default.unlinkSync(`${(_a = _req.file) === null || _a === void 0 ? void 0 : _a.path}`);
+                    res.status(400).json({
+                        message: 'Usuario no encontrado',
+                        status: 400,
+                    });
+                }
+            }
+            else {
+                fs_1.default.unlinkSync(`${(_b = _req.file) === null || _b === void 0 ? void 0 : _b.path}`);
+                res.status(400).json({
+                    message: 'no es un archivo pdf',
+                    status: 400,
+                });
+            }
         });
         //guardar cliente
         this.SaveClient = (_req, res) => __awaiter(this, void 0, void 0, function* () {
--- src/api/controllers/ImpulsaController.ts
+++ src/api/controllers/ImpulsaController.ts
@@ -9,80 +9,6 @@ var path = require('path');
 /** My class of user controller */
 class ImpulsaController {
 
-    //probando la subida de archivos pdf
-    public Savefiles = async(_req: Request, res : Response)=>{
-
-        res.set('Access-Control-Allow-Origin', '*');
-        // let file = _req.files;
-        // var file:Array<any> = _req.files as any
-
-        // var urlFile:Array<any>=[] 
-
-        // console.log(urlFile)
-
-        // file.forEach(item=>{ 
-        // urlFile.push(
-        // {
-        //     "url":item.path,
-        // });
-        // });
-
-        // console.log(urlFile)
-        var file = _req.file;
-        
-
-        if (!file) {
-            const error = new Error('Please upload a file');
-            return error;
-        }else if(file.mimetype === 'application/pdf'){
-            /** search Number phone in the data base */
-            const isUserExist = await UsersModel.findOne({ username: _req.params.phoneNumber });
-
-            if (isUserExist) {
-                //instantiating the model for save data
-                const user = new InsurancePoliciesModel({
-                    insurerName: _req.body.insurerName,
-                    policyNumber: _req.body.policyNumber,
-                    policyType: _req.body.policyType,
-                    effectiveDate: Date.now(),
-                    expirationDate: Date.now(),
-                    status: _req.body.status,
-                    fileUrl: file.filename,
-                    clientId:isUserExist.clientId
-                });
-
-                try {
-
-                    //save data
-                    const savedUser = await user.save();
-
-                    //send request exit
-                    res.status(200).json({
-                        message: 'Poliza registrada',
-                        file
-                    });
-                } catch (error) {
-                    res.status(404).json({
-                        error,
-                        status: 404
-                    });
-                }
-            }else{
-                fs.unlinkSync(`${_req.file?.path}`);
-                res.status(400).json({
-                    message: 'Usuario no encontrado',
-                    status: 400,
-                });
-            }
-        }else{
-            fs.unlinkSync(`${_req.file?.path}`);
-            res.status(400).json({
-                message: 'no es un archivo pdf',
-                status: 400,
-            });
-        }
-    }
-
     //ver pdf de un cliente
 
     public ViewFile = async(_req : Request, res : Response)=>{
@@ -150,6 +76,80 @@ class ImpulsaController {
                 res.json("success")
             }    
         });
+    }
+    
+    //probando la subida de archivos pdf
+    public Savefiles = async(_req: Request, res : Response)=>{
+
+        res.set('Access-Control-Allow-Origin', '*');
+        // let file = _req.files;
+        // var file:Array<any> = _req.files as any
+
+        // var urlFile:Array<any>=[] 
+
+        // console.log(urlFile)
+
+        // file.forEach(item=>{ 
+        // urlFile.push(
+        // {
+        //     "url":item.path,
+        // });
+        // });
+
+        // console.log(urlFile)
+        var file = _req.file;
+        
+
+        if (!file) {
+            const error = new Error('Please upload a file');
+            return error;
+        }else if(file.mimetype === 'application/pdf'){
+            /** search Number phone in the data base */
+            const isUserExist = await UsersModel.findOne({ username: _req.params.phoneNumber });
+
+            if (isUserExist) {
+                //instantiating the model for save data
+                const user = new InsurancePoliciesModel({
+                    insurerName: _req.body.insurerName,
+                    policyNumber: _req.body.policyNumber,
+                    policyType: _req.body.policyType,
+                    effectiveDate: Date.now(),
+                    expirationDate: Date.now(),
+                    status: _req.body.status,
+                    fileUrl: file.filename,
+                    clientId:isUserExist.clientId
+                });
+
+                try {
+
+                    //save data
+                    await user.save();
+
+                    //send request exit
+                    res.status(200).json({
+                        message: 'Poliza registrada',
+                        file
+                    });
+                } catch (error) {
+                    res.status(404).json({
+                        error,
+                        status: 404
+                    });
+                }
+            }else{
+                fs.unlinkSync(`${_req.file?.path}`);
+                res.status(400).json({
+                    message: 'Usuario no encontrado',
+                    status: 400,
+                });
+            }
+        }else{
+            fs.unlinkSync(`${_req.file?.path}`);
+            res.status(400).json({
+                message: 'no es un archivo pdf',
+                status: 400,
+            });
+        }
     }
 
     //guardar cliente
